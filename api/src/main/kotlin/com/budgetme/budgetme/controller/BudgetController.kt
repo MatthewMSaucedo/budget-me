c15d6b87326d1432df6d1409a849f13ff21af2e3
Matthew Saucedo <themattsaucedo@gmail.com>
2020-03-14 14:25:33 -0400

    Update README with site link

--- api/src/main/kotlin/com/budgetme/budgetme/controller/BudgetController.kt
+++ api/src/main/kotlin/com/budgetme/budgetme/controller/BudgetController.kt
@@ -6,23 +6,24 @@ import com.budgetme.budgetme.repository.ExpenseBudgetRepository
 import com.budgetme.budgetme.util.minus
 import org.springframework.web.bind.annotation.*
 
+@CrossOrigin(origins = ["*"])
 @RequestMapping("api/budget")
 @RestController
 class BudgetController (val expenseBudgetRepository: ExpenseBudgetRepository) {
 
-  @CrossOrigin(origins = ["http://localhost:3000", "http://budget-me.s3-website-us-east-1.amazonaws.com/"])
+  @CrossOrigin(origins = ["*"])
   @PostMapping(path = ["charge"])
   public fun charge(@RequestBody chargeRequest: ChargeRequest): ExpenseBudget {
     return expenseBudgetRepository.save(chargeRequest.expenseBudget - chargeRequest.cost)
   }
 
-  @CrossOrigin(origins = ["http://localhost:3000", "http://budget-me.s3-website-us-east-1.amazonaws.com/"])
+  @CrossOrigin(origins = ["*"])
   @GetMapping(path = ["listExpenseBudget"])
   public fun listExpenseBudget(): List<ExpenseBudget> {
     return expenseBudgetRepository.findAll();
   }
 
-  @CrossOrigin(origins = ["http://localhost:3000", "http://budget-me.s3-website-us-east-1.amazonaws.com/"])
+  @CrossOrigin(origins = ["*"])
   @PostMapping(path = ["createExpenseBudget"])
   public fun createExpenseBudget(@RequestBody expenseBudget: ExpenseBudget): ExpenseBudget {
     println(expenseBudget)
